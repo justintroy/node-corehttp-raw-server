4a35e3e05b653ccc08977883f0210ead508ef715
Justin Troy Marilao <xermasterz@gmail.com>
2021-12-16 08:45:51 +0800

    routesHandler.js refactor

--- src/routesHandler.js
+++ src/routesHandler.js
@@ -1,11 +1,11 @@
 import { readdirSync } from "fs";
 
 export const routesHandler = (targetPath) => {
-    return new Promise(async (resolve, reject) => {
-        const files = readdirSync("./routes").filter((file) =>
-            file.endsWith(".js")
-        );
+    const files = readdirSync("./routes").filter((file) =>
+        file.endsWith(".js")
+    );
 
+    return new Promise(async (resolve, reject) => {
         for (const file of files) {
             const route =
                 file === "root.js" ? "/" : "/" + file.slice(0, file.length - 3);
